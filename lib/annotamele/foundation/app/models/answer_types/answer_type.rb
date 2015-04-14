78ebb6e6af9381fe79411a97cefcb2d7471754e6
alexlag <bo4onok@gmail.com>
2015-04-14 15:35:45 +0300

    better AnswerType

--- lib/annotamele/foundation/app/models/answer_types/answer_type.rb
+++ lib/annotamele/foundation/app/models/answer_types/answer_type.rb
@@ -3,7 +3,31 @@ class AnswerType < ActiveRecord::Base
 
   self.inheritance_column = :type
 
-  # def self.types
-  #   %w(SingleLabel MultiLabel)
-  # end
+  class_attribute :custom_fields_list
+  self.custom_fields_list = []
+
+  def self.custom_fields(*args)
+    args.each do |arg|
+      custom_fields_list << arg
+      class_eval("def #{arg}; body[:#{arg}];end")
+      class_eval("def #{arg}=(val);body[:#{arg}]=val;end")
+    end
+  end
+
+  def view_partial
+    {
+      partial: "answer_types/#{self.class.to_s.underscore}",
+      locals: export
+    }
+  end
+
+  def export
+    custom_fields_list.each_with_object(type: self.class.name) do |field, total|
+      total[field] = body[field]
+    end
+  end
+
+  def validation(_answer)
+    true
+  end
 end
--- lib/annotamele/foundation/app/models/answer_types/single_label.rb
+++ lib/annotamele/foundation/app/models/answer_types/single_label.rb
@@ -1,34 +1,5 @@
 class SingleLabel < AnswerType
-  def text=(text)
-    body[:text] = text
-  end
-
-  def text
-    body[:text]
-  end
-
-  def options=(options)
-    body[:options] = options
-  end
-
-  def options
-    body[:options]
-  end
-
-  def view_partial
-    {
-      partial: 'answer_types/single_label',
-      locals: export
-    }
-  end
-
-  def export
-    {
-      type: self.class.name,
-      text: text,
-      options: options
-    }
-  end
+  custom_fields :text, :options
 
   def validation(answer)
     return false unless answer.is_a?(Array) && answer.length < 2
